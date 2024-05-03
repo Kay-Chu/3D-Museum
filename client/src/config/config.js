1d34bced2afa27fe9905aa543b63a9cd419fe85b
Kaying <39971215+Kay-Chu@users.noreply.github.com>
2024-05-03 14:14:10 +0800

    Path debugged

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,7 @@
 const config = {
   development: {
-    backendUrl: "http://localhost:8080/api/v1/dalle",
+    backendUrl: "http://museum.k-chu.com/api/v1/dalle",
+    //backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
     backendUrl: "https://devswag.onrender.com/api/v1/dalle",
